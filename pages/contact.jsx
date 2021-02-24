701ba95e13c19ece3e7ddf295aaa30ab35508434
Willem Prins <willemprins93@gmail.com>
2021-02-24 16:45:44 +0000

    add test contact form

---
+++ pages/contact.jsx
@@ -0,0 +1,43 @@
+import Head from "next/head";
+import styles from "../styles/About.module.scss";
+
+const About = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Contact</title>
+      </Head>
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Role:{" "}
+            <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default About;
