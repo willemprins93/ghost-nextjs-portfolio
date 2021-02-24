b10b756a743c6525659f9fa4b12cb7126707998e
Willem Prins <willemprins93@gmail.com>
2021-02-24 17:23:25 +0000

    update form

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,31 +1,82 @@
 import Head from "next/head";
+import React from "react";
 import styles from "../styles/About.module.scss";
 
-const About = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Contact</title>
-      </Head>
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>Name:</label>
-          <input type="text" name="name" />
-        </p>
-        <p>
-          <label>Email:</label>
-          <input type="email" name="email" />
-        </p>
-        <p>
-          <label>Message:</label>
-          <textarea name="message"></textarea>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    </div>
-  );
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 };
 
-export default About;
+class Contact extends React.Component {
+  state = {
+    name: "",
+    email: "",
+    message: "",
+  };
+
+  handleSubmit = (e) => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state }),
+    })
+      .then(() => console.log("success"))
+      .catch((error) => console.log(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, message } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Contact</title>
+        </Head>
+        <form
+          name="contact"
+          method="POST"
+          onSubmit={this.handleSubmit}
+          action="/about"
+        >
+          <input type="hidden" name="contact-form" value="contact" />
+          <p>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+            />
+          </p>
+          <p>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+          </p>
+          <p>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              name="message"
+              value={message}
+              onChange={this.handleChange}
+            />
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Contact;
