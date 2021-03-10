f27253c748006d0da7ffa7f90d4aec01f236a7b2
Willem Prins <willemprins93@gmail.com>
2021-03-10 14:46:28 +0000

    add google analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -8,6 +8,21 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <link rel="icon" href="/wpportfolio_favicon.png" type="image/png" />
         <meta lang="en-EN" />
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-B3SY0XE0BQ"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-B3SY0XE0BQ');`,
+          }}
+        ></script>
       </Head>
       <Layout>
         <Component {...pageProps} />
